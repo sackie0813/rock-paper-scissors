504b77e197b191875ff77a6db9f5ccac9f4e62a6
sackie0813 <jsjsone0813@gmail.com>
2024-07-31 19:25:30 -0400

    added game logic, added getComputerChoice and getPlayerChoice functions. Added a game loop

---
+++ script.js
@@ -0,0 +1,56 @@
+let playerScore = 0;
+let computerScore = 0;
+
+function getComputerChoice () {
+    let choice = Math.floor (Math.random()*3) + 1;
+    if (choice === 1) {
+        return "rock";
+    } else if (choice === 2) {
+        return "paper";
+    } else if (choice === 3) {
+        return "scissors";
+    }
+}
+
+function getPlayerChoice() {
+    let choice = prompt("Rock, paper, or scissors?").toLowerCase();
+    return choice;
+}
+
+function playRound (playerChoice , computerChoice) {
+    if (playerChoice === computerChoice) {
+        return "Draw! Neither player has been given a point";
+    } else if (playerChoice === "rock" && computerChoice === "scissors") {
+        playerScore++;
+        return "Player Wins! Player has been given a point";
+    } else if (playerChoice === "rock" && computerChoice === "paper") {
+        computerScore++;
+        return "Computer Wins! Computer has been given a point";
+    } else if (playerChoice === "scissors" && computerChoice == "rock") {
+        computerScore++;
+        return "Computer Wins! Computer has been given a point";
+    } else if (playerChoice === "scissors" && computerChoice == "paper") {
+        playerScore++;
+        return "Player Wins! Player has been given a point";
+    } else if (playerChoice === "paper" && computerChoice == "rock") {
+        playerScore++;
+        return "Player Wins! Player has been given a point";
+    } else if (playerChoice === "paper" && computerChoice == "scissors") {
+        computerScore++;
+        return "Computer Wins! Computer has been given a point";
+    }
+}
+
+while (playerScore <= 5 || computerScore < 5) {
+    console.log(playRound(getPlayerChoice() , getComputerChoice()));
+    if (playerScore === 5) {
+        console.log("Player wins the war! 5 points");
+        playerScore, computerScore ===0;
+        break;
+
+    } else if (computerScore === 5) {
+        console.log ("Computer wins the war! 5 points");
+        playerScore, computerScore === 0;
+        break;
+    }
+}
